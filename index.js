2ebb91c80b8dee1a4c3ca0223e51dcd45f94c178
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 21:00:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var windowName = 'userConsole';
 var popUp = window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
 if (popUp == null || typeof(popUp) == 'undefined') {
-  while (true) {
     alert("Thicc Katz")
   }
 } else {
