41e5357139a1bfbef6de1ac51ac884921a90557e
Dr3w0623 <34252192+Dr3w0623@users.noreply.github.com>
2017-12-19 15:42:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ function detectPopupBlocker() {
       var strWindowFeatures = "location=yes,height=1200,width=1400,scrollbars=yes,status=yes";
       var URL = "http://kplibrary.cf/" + location.href;
       var win = window.open(URL, "_blank", strWindowFeatures);
-      setTimeout(function() {
-        for (var i = 0; i === i; i++) {}
-      }, 5);
     }
   }
 }
